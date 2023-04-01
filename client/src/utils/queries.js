3dfea0f935457a2b2dbd06e1c0e7b5d7ae1ed4fc
Calvin Chern <calvin.chern24@gmail.com>
2023-04-01 16:40:16 -0400

    Add queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+// export queries for me
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
