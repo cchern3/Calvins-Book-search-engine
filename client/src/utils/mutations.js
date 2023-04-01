f6edfcff0f11c6823360c0fb3d00208f80fc8477
Calvin Chern <calvin.chern24@gmail.com>
2023-04-01 16:38:52 -0400

    Add mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+
+//exporting mutation for login users
+export const LOGIN_USER = gql`
+  mutation login(
+    $email: String!
+    $password: String!
+  ) {
+    login(
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+//exporting adduser  content
+export const ADD_USER = gql`
+  mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook: AddBook!) {
+    saveBook(newBook: $newBook) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
